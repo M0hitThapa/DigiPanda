2663bbd4da7b43937fd85afd4c960d9e55409801
Mohit Thapa <thapamohit2058@gmail.com>
2025-03-04 00:45:33 +0545

    .

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,7 @@
+const NavItem = () => {
+    return (
+        <h1>Hello</h1>
+    )
+}
+
+export default NavItem
---
+++ src/config/index.ts
@@ -0,0 +1,46 @@
+export const PRODUCT_CATRGORIES = [
+    {
+        label:"UI Kits",
+        value:"ui_kits" as const,
+        featured: [
+            {
+                name:"Editor picks",
+                href:"#",
+                imageSrc:"/nav/ui-kits/ui1.jpg"
+            },
+            {
+                name:"New Arrivals",
+                href:"#",
+                imageSrc:"/nav/ui-kits/ui2.jpg"
+            },
+            {
+                name:"Best Sellers",
+                href:"#",
+                imageSrc:"/nav/ui-kits/ui3.jpg"
+            },
+        ]
+    },
+
+
+    {
+        label:"Icons",
+        value:"icons" as const,
+        featured: [
+            {
+                name:"Favourite Icon Picks",
+                href:"#",
+                imageSrc:"/nav/icons/icon1.jpeg"
+            },
+            {
+                name:"New Arrivals",
+                href:"#",
+                imageSrc:"/nav/icons/icon2.jpeg"
+            },
+            {
+                name:"Bestselling Icons",
+                href:"#",
+                imageSrc:"/nav/icons/icon3.jpeg"
+            },
+        ]
+    }
+]
