1e78482913559bf6d27ae7b96a011e9e74e6b60d
Mohit Thapa <thapamohit2058@gmail.com>
2025-03-07 18:59:16 +0545

    added navitems

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,13 +1,21 @@
 "use client"
 
 import { PRODUCT_CATRGORIES } from "@/config"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import NavItem from "./NavItem"
+import { useOnClickOutside } from "@/hooks/use-on-click-outside"
 
 
 const NavItems = () => {
     const [activeIndex, setActiveIndex] = useState<null | number>(null)
-    return<div className="flex gap-4 h-full">
+
+    const isAnyOpen = activeIndex !== null
+
+const navRef = useRef<HTMLDivElement | null>(null)
+
+useOnClickOutside(navRef, () => setActiveIndex(null))
+
+    return<div className="flex gap-4 h-full" ref={navRef}>
 {PRODUCT_CATRGORIES.map((category, i) => {
     const handleOpen = () => {
         if(activeIndex === i) {
@@ -19,7 +27,9 @@ const NavItems = () => {
 
     const isOpen = i === activeIndex
     return(
-        <NavItem />
+        <NavItem category={category} handleOpen={handleOpen} isOpen={isOpen}
+        key={category.value}
+        isAnyOpen={isAnyOpen} />
     )
 })}
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { Icons } from "./Icons"
+import NavItems from "./NavItems"
 
 const Navbar = () => {
     return(
         <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
             <header className="relative bg-white">
                 <MaxWidthWrapper>
+                
+                    
                     <div className="border-b border-gray-200">
                         <div className="flex h-16 items-center">
                             {/*Todo: Mob Nav */}
@@ -15,7 +18,13 @@ const Navbar = () => {
                                 <Link href='/'>
                                 <Icons.logo className="h-10 w-10" />
                                 </Link>
+                                
+                                
                             </div>
+                            <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
+                <NavItems />
+              </div>
+                            
                             <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch"></div>
                         </div>
                     </div>
